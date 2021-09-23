135755e21b0979eb3cf7ec615494ce83be46744e
Naomi Rodriguez <naomirodriguez@naomis-mbp.t-com.me>
2021-09-23 14:13:14 +0200

    Fixed issue with sign out by re routing the route to users/

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   def new; end
 
+  def show;
+  end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   default_url_options :host => "localhost:3000"
 
-  devise_for :users, path: 'auth', controllers: { sign_in: 'login', sign_out: 'logout'}
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session  
+  end
   resources :metrics, only: [:index, :show, :create]
   root to: "metrics#index"
  
