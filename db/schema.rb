7c71d1fa19f9da26ea8e26013d016f09b2d81aee
Naom-Roads <nrrodrig@gmail.com>
2021-10-07 10:19:40 +0200

    Added associations between Metrics, Workstations and Users

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_10_05_145429) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "plsql"
 
   create_table "metrics", force: :cascade do |t|
     t.string "category"
