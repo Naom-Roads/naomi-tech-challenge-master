f8663bca5a2d5755b18d773e86f1d66e180a1a70
Naom-Roads <nrrodrig@gmail.com>
2021-10-19 16:43:50 +0200

    Made some changes to views for search

--- app/controllers/workstations_controller.rb
+++ app/controllers/workstations_controller.rb
@@ -2,11 +2,23 @@ class WorkstationsController < ApplicationController
 
   def index
     @workstations = Workstation.all
+    #search goes here
   end
 
   def show
+
     @workstation = Workstation.find_by(id: params[:id])
     @metrics = Metrics.where(workstation_id: @workstation.id)
+
   end
+
+
+private
+
+  def workstation_params
+    params.require(:workstation).permit(:name, :id, :search)
+  end
+
+
 
 end
